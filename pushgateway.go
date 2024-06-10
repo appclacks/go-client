ab8dfe4619e9ee88f7a4019ac8516c7c717a6b2b
mcorbin <corbin.math@gmail.com>
2024-06-10 20:33:35 +0200

    pushgateway: add first payload

---
+++ pushgateway.go
@@ -0,0 +1,25 @@
+package client
+
+import "time"
+
+type PushgatewayMetric struct {
+	ID          string            `json:"id"`
+	Name        string            `json:"name"`
+	Description string            `json:"description,omitempty"`
+	Labels      map[string]string `json:"labels,omitempty"`
+	TTL         string            `json:"ttl"`
+	Type        string            `json:"type"`
+	CreatedAt   time.Time         `json:"created_at"`
+	ExpiresAt   time.Time         `json:"expires_at"`
+	Value       float32           `json:"value"`
+}
+
+type CreateOrUpdatePushgatewayMetricInput struct {
+	Name        string            `json:"name" validate:"required,max=255,min=1"`
+	Description string            `json:"description,omitempty"`
+	Labels      map[string]string `json:"labels" description:"Healthcheck labels" validate:"dive,keys,max=255,min=1,endkeys,max=255,min=1"`
+	TTL         string            `json:"ttl"`
+	Type        string            `json:"type"`
+	ExpiresAt   time.Time         `json:"expires_at"`
+	Value       float32           `json:"value" validate:"required"`
+}
